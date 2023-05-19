1e7f78e859d4001f389af8db9ce6775daabaa93c
Hiteshwar <hemant.kaimwal96@gmail.com>
2023-05-19 12:51:33 +0530

    basic component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
 import './App.css';
+import Navbar from "./Navbar";
+import Filter from "./Filter";
+import Cards from "./Cards";
 
 function App() {
   return (
     <div className="App">
-      app
+       <Navbar/>
+
+       <Filter/>
+
+       <Cards/>
     </div>
   );
 }
